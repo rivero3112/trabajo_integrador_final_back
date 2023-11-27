2ce3a2d96389ba5a517db92d8879e6aa95412e8a
NotFoundSN <julio.maldonado.sn@gmail.com>
2023-11-27 17:41:42 -0300

    ADD inicializacion del proyecto de flask, con conexion a la base de datos

---
+++ dbModels/model.py
@@ -0,0 +1,12 @@
+# defino la tabla
+class Producto(db.Model):   # la clase Producto hereda de db.Model    
+    id=db.Column(db.Integer, primary_key=True)   #define los campos de la tabla
+    nombre=db.Column(db.String(100))
+    precio=db.Column(db.Integer)
+    stock=db.Column(db.Integer)
+    imagen=db.Column(db.String(400))
+    def __init__(self,nombre,precio,stock,imagen):   #crea el  constructor de la clase
+        self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
+        self.precio=precio
+        self.stock=stock
+        self.imagen=imagen
---
+++ readme.md
@@ -0,0 +1,14 @@
+para ejecutar usar
+
+
+1) .venv\Scripts\activate
+
+2) flask --app app run
+
+
+----------------------------------------
+
+problemas de ejecucion
+
+1) Set-ExecutionPolicy -ExecutionPolicy Unrestricted -scope LocalMachine
+2) Set-ExecutionPolicy -ExecutionPolicy Unrestricted -scope CurrentUser
